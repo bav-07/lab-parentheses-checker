39ed9b280d2b01f89a1942743efbe0bb01cfc2a5
Bavaharsan Nagarajah <bavaharsan.nagarajah@gmail.com>
2022-11-24 14:38:47 +0000

    first commit

---
+++ src/main/java/ParenthesesChecker.java
@@ -0,0 +1,22 @@
+public class ParenthesesChecker {
+
+    // write a method called checkParentheses()
+    // takes in a String
+    // returns a boolean
+
+    // return true if the string contains "correct" pairs of parentheses
+    // return false if not!
+    // situations to return false:
+        // if there's an unequal amount of opening and closing parentheses of a particular type
+        // closing bracket at start, opening one at the end
+        // opening bracket followed by 2 closing brackets
+
+    public ParenthesesChecker() {
+
+    }
+
+    public boolean checkParentheses(String string) {
+        return false;
+    }
+
+}
